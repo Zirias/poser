614d87239db72d8b59fa5e49f578748c46696da7
Felix Palmen <felix@palmen-it.de>
2023-05-27 20:47:16 +0200

    Update docs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var index =
 [
-    [ "libposercore", "index.html#autotoc_md1", [
-      [ "Quick start", "index.html#autotoc_md2", null ]
+    [ "Dynamic library versioning", "index.html#autotoc_md1", null ],
+    [ "libposercore", "index.html#autotoc_md2", [
+      [ "Quick start", "index.html#autotoc_md3", null ],
+      [ "Threading model", "index.html#autotoc_md4", null ]
     ] ]
 ];
