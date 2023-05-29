ee866c53e1c89b5918638295afd70f4bb29bb622
Felix Palmen <felix@palmen-it.de>
2023-05-29 21:38:28 +0200

    update docs

--- class_p_s_c___server.js
+++ class_p_s_c___server.js
@@ -4,5 +4,7 @@ var class_p_s_c___server =
     [ "PSC_Server_clientDisconnected", "class_p_s_c___server.html#ac587f04f84814d3bef8cda60bdc0a7bb", null ],
     [ "PSC_Server_createTcp", "class_p_s_c___server.html#afc755a461bbbc7d102d285b08067e926", null ],
     [ "PSC_Server_createUnix", "class_p_s_c___server.html#abb5ce643c268a6a7aec8cb0e731405ea", null ],
-    [ "PSC_Server_destroy", "class_p_s_c___server.html#a4062c09551b15328f8b6b1685d4c3a5c", null ]
+    [ "PSC_Server_destroy", "class_p_s_c___server.html#a4062c09551b15328f8b6b1685d4c3a5c", null ],
+    [ "PSC_Server_disable", "class_p_s_c___server.html#a01cfb7d7b2cf8b43a5a104f864711159", null ],
+    [ "PSC_Server_enable", "class_p_s_c___server.html#a892197bff8672929eb6cfc2fd3e0812d", null ]
 ];
