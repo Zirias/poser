1a89474692aa1610c899c12dcf95ffaedcb5d0d0
Felix Palmen <felix@palmen-it.de>
2024-07-11 11:04:46 +0200

    core.mk: Add required librt for timers

--- src/lib/core/core.mk
+++ src/lib/core/core.mk
@@ -35,6 +35,7 @@ posercore_HEADERS_INSTALL:=	core \
 				core/util \
 				decl
 
+posercore_LIBS:=		rt
 posercore_LDFLAGS:=		-pthread
 posercore_HEADERDIR:=		include$(PSEP)poser
 posercore_HEADERTGTDIR:=	$(includedir)$(PSEP)poser
