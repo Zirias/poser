3aba8f18f3fe4ac8787336b527d2c77a44936bd2
Felix Palmen <felix@palmen-it.de>
2025-04-14 14:21:41 +0200

    xxHash: Hide symbols from library interface
    Hide symbols of the bundled xxHash implementation, so they don't clash
    with a full-featured xxHash library that might be linked.

--- src/lib/core/xxhash.h
+++ src/lib/core/xxhash.h
@@ -1,10 +1,14 @@
 #ifndef POSER_CORE_INT_XXHASH_H
 #define POSER_CORE_INT_XXHASH_H
 
+#include <poser/decl.h>
+
 #ifdef POSER_XXH_IMPL
 
 #  define XXH_NO_STDLIB
 #  define XXH_NO_STREAM
+#  define XXH_PRIVATE_API
+#  define XXH_PUBLIC_API SOLOCAL
 
 #else
 
--- src/lib/core/xxhx86.c
+++ src/lib/core/xxhx86.c
@@ -1,5 +1,7 @@
 #ifdef HAVE_XXHX86
+#pragma GCC visibility push (hidden)
 #include "contrib/xxHash/xxh_x86dispatch.c"
+#pragma GCC visibility pop
 #else
 #include <poser/decl.h>
 SOLOCAL char psc__dummy;
